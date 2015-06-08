f5cf63719179af651046361e03efad8d80f198c2
Vinod Pankajakshan <vinu.pankajakshan@gmail.com>
2015-06-07 23:21:05 -0400

    clean up unused methods/codes in scrap_cws.py

--- scrap_cws.py
+++ scrap_cws.py
@@ -2,8 +2,6 @@
 
 import re
 import requests
-# import json
-from BeautifulSoup import BeautifulSoup
 
 from utils import find_index
 
@@ -25,18 +23,6 @@ def main():
     menu_list = get_table_of_contents()
     make_table_of_contents(menu_list)
     return table_of_contents
-    # beautyfulrasam()
-
-
-def beautyfulrasam():
-    html_content = requests.get(url)
-    print html_content
-    soup = BeautifulSoup(html_content.text)
-    print "vinod"
-    # rytvidu = soup
-    script = soup.findall('script').string
-    print script
-
 
 def get_table_of_contents():
     html_content = requests.get(url)
@@ -84,17 +70,7 @@ def make_table_of_contents(menu_list):
 
             count += 1
 
-            # if head == "Volume_2":
-            #     FLAG = True
-            # elif head == "Volume_3":
-            #     FLAG = False
-
-            # if FLAG:
             build_dictionary(tree_pos, head, head_desc, topic_url)
-            # print tree_pos, 3*'=', head, 3*'=', head_desc, 3*'=', topic_url
-            # if count > 56 and count < 65:
-            # print tree_pos, 3*'=', head, 3*'=', head_desc, 3*'=', topic_url
-            # print json.dumps(table_of_contents)
 
 
 def build_dictionary(mnu_pos, head, head_desc, topic_url):
@@ -115,16 +91,14 @@ def build_dictionary(mnu_pos, head, head_desc, topic_url):
         else:
             mnu_pos = fix_menu_level(mnu_pos)
             add_chapters(mnu_pos, head, head_desc, topic_url)
-
-        # print table_of_contents
-        # print mnu_pos, 3*'=', PRE_MNU_POS, 3*'=', head, 3*'='
-        # , head_desc, 3*'=', topic_url
-        # print mnu_pos, 3*'=', head, 3*'=', head_desc, 3*'=', topic_url
     except Exception as ex:
         raise ex
 
 
 def fix_menu_level(mnu_pos):
+    '''
+    little hack to fix menu ordering in the source website
+    '''
     global PRE_MNU_POS
 
     if mnu_pos == PRE_MNU_POS:
@@ -141,7 +115,6 @@ def fix_menu_level(mnu_pos):
 
 
 def add_chapters(mnu_pos, head, head_desc, topic_url):
-    # global PRE_MNU_POS
     arr = [head_desc, topic_url, {}]
 
     try:
@@ -150,37 +123,15 @@ def add_chapters(mnu_pos, head, head_desc, topic_url):
             if mnu_pos == 2:
                 global chap
                 chap = head
-                # PRE_MNU_POS = mnu_pos
                 table_of_contents[volume][2][head] = arr
             elif mnu_pos == 3:
                 global sub_chap
                 sub_chap = head
-                # PRE_MNU_POS = mnu_pos
                 table_of_contents[volume][2][chap][2][head] = arr
             elif mnu_pos == 4:
-                # PRE_MNU_POS = mnu_pos
                 table_of_contents[volume][2][chap][2][sub_chap][2][head] = arr
     except Exception as ex:
         raise ex
-
-def fix_num(PRE_MNU_POS, mnu_pos):
-    '''
-    little hack to workaround bogus menu sequencing in the source site
-    '''
-
-    global is_fix_mnu_pos
-
-    if PRE_MNU_POS != mnu_pos and (PRE_MNU_POS + 1) != mnu_pos:
-        is_fix_mnu_pos = True
-        return PRE_MNU_POS + 1
-    elif PRE_MNU_POS != mnu_pos and (PRE_MNU_POS - 1) >= mnu_pos:
-        is_fix_mnu_pos = False
-        return mnu_pos
-    elif PRE_MNU_POS == mnu_pos:
-        return mnu_pos - 1
-
-    if is_fix_mnu_pos:
-        return PRE_MNU_POS
 
 
 if __name__ == '__main__':
